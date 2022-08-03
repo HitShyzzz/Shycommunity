24cde34c46ec890948e53d16f82fdebfaaaa0743
ShyZzz <2497141901@qq.com>
2022-08-03 23:22:47 +0800

    8.3

---
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -0,0 +1,29 @@
+package com.nowcoder.community;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: shy
+ * @Date: 2022/08/03/16:01
+ * @Description:
+ */
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTests {
+    private static final Logger logger =LoggerFactory.getLogger(LoggerTests.class);
+
+    @Test
+    public void testLogger(){
+        System.out.println(logger.getName());
+        logger.debug("debug log");
+        logger.info("debug log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+}
---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,44 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.apache.catalina.loader.WebappClassLoader;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: shy
+ * @Date: 2022/08/03/17:56
+ * @Description:
+ */
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("2497141901@qq.com","TEST","hello ,spring email!");
+    }
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+        mailClient.sendMail("2497141901@qq.com","HTML",content);
+    }
+
+}
